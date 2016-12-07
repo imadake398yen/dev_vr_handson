3e79ea527cce8803cd9fc4a406b533fe908b662a
falcon <imadake398yen@gmail.com>
2016-12-07 21:53:10 +0900

    オブジェクトを選択する

---
+++ Assets/VR_Hands_On/Scripts/ObjectDescriptionController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ObjectDescriptionController
+:SingletonMonoBehaviour<ObjectDescriptionController> {
+
+	[SerializeField] private Canvas canvas;
+	[SerializeField] private Text descriptionLabel;
+	private PlayerController player;
+	private SelectableObject target;
+
+	private float remainingDesplayTime;
+	const float desplayTime = 2.5f;
+
+	private void Update () {
+		
+		if (player == null) { 
+			if (PlayerController.instance == null) return;
+			else player = PlayerController.instance;
+		}
+		canvas.enabled = target == null ? false : true;
+		if (!canvas.enabled) return;
+
+		transform.LookAt(player.transform);
+
+		if (target != null) {
+			transform.transform.position = player.transform.position 
+				+ (target.transform.position - player.transform.position).normalized * 2.0f;
+		}
+
+		remainingDesplayTime -= Time.deltaTime;
+		if (remainingDesplayTime <= 0) {
+			canvas.enabled = false;
+			target = null;
+		}
+
+	}
+
+	public void SetTarget (SelectableObject t) {
+		if (target != t) {
+			remainingDesplayTime = desplayTime;
+			target = t;
+			StartCoroutine(PutText(t.description));
+		}
+	}
+
+	private IEnumerator PutText (string text) {
+		print(text);
+		for (int i=0; i<text.Length; i++) {
+			descriptionLabel.text = text.Substring(0, i+1);
+			yield return new WaitForSeconds(0.08f);
+		}
+	}
+}
---
+++ Assets/VR_Hands_On/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController
+: SingletonMonoBehaviour<PlayerController> {
+
+	[SerializeField] private Transform diveCameraOrigin;
+	[SerializeField] private Image lookGaugeImage;
+
+	const float lookGaugeSpeed = 0.02f;
+
+	private void Update () {
+		Ray ray = new Ray(diveCameraOrigin.position, diveCameraOrigin.forward);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)) {
+			var selectable = hit.transform.GetComponent<SelectableObject>();
+			if (selectable != null) {
+				lookGaugeImage.fillAmount += lookGaugeSpeed;
+				if (lookGaugeImage.fillAmount == 1.0f) {
+					ObjectDescriptionController.instance.SetTarget(selectable);
+				}
+			} else {
+				lookGaugeImage.fillAmount -= lookGaugeSpeed * 3.0f;
+			}
+		} else {
+			lookGaugeImage.fillAmount -= lookGaugeSpeed * 3.0f;
+		}
+	}
+
+}
